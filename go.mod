9b82525d3d68710c63a675cffbe9666d34c4e15d
Matthew Hilton <mj.hilton@outlook.com>
2024-12-01 21:39:35 +1000

    refactor to use goroutines

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module matt/aoc
 
 go 1.23.3
 
-require golang.org/x/exp v0.0.0-20241108190413-2d47ceb2692f // indirect
+require golang.org/x/exp v0.0.0-20241108190413-2d47ceb2692f
--- main.go
+++ main.go
@@ -39,28 +39,25 @@ func day_1() {
 	check(err)
 
 	// Split into pairs and remove any garbage.
+	// Using goroutines, for fun.
 	data_as_string := string(data)
 	lines := strings.Split(data_as_string, "\n")
+
+	var channels []chan [2]int
+	for _, line := range lines {
+		channels = append(channels, get_pair(line))
+	}
 
 	var lefts []int
 	var rights []int
 
-	for _, line := range lines {
-		parts := strings.Split(line, "   ")
+	for _, c := range channels {
+		v, ok := <-c
 
-		if len(parts) != 2 {
-			continue
+		if ok {
+			lefts = append(lefts, v[0])
+			rights = append(rights, v[1])
 		}
-
-		left, err1 := strconv.Atoi(strings.TrimSpace(parts[0]))
-		right, err2 := strconv.Atoi(strings.TrimSpace(parts[1]))
-
-		if err1 != nil || err2 != nil {
-			continue
-		}
-
-		lefts = append(lefts, left)
-		rights = append(rights, right)
 	}
 
 	// Sort each side.
@@ -75,17 +72,54 @@ func day_1() {
 	fmt.Println(total)
 
 	// Part 2  - get similarity score.
+	// With channels, just to be interesting.
 	similarity := 0
+	similarity_channel := make(chan int)
 	for _, left := range lefts {
-		var occurrences = 0
+		go calculate_similarity(left, rights, similarity_channel)
+	}
 
-		for _, right := range rights {
-			if left == right {
-				occurrences += 1
-			}
+	for range len(lefts) {
+		similarity += <-similarity_channel
+	}
+	close(similarity_channel)
+
+	fmt.Println(similarity)
+}
+
+func get_pair(value string) chan [2]int {
+	c := make(chan [2]int)
+
+	go func() {
+		parts := strings.Split(value, "   ")
+		if len(parts) != 2 {
+			close(c)
+			return
 		}
 
-		similarity += occurrences * left
+		left, err1 := strconv.Atoi(strings.TrimSpace(parts[0]))
+		right, err2 := strconv.Atoi(strings.TrimSpace(parts[1]))
+
+		if err1 != nil || err2 != nil {
+			close(c)
+			return
+		}
+
+		c <- [2]int{left, right}
+	}()
+
+	return c
+}
+
+/*
+Finds the number of ocurrences of value in data, and sends back the number to the given channel.
+*/
+func calculate_similarity(value int, data []int, c chan int) {
+	occurrences := 0
+	for _, data_value := range data {
+		if value == data_value {
+			occurrences += 1
+		}
 	}
-	fmt.Println(similarity)
+	c <- occurrences * value
 }
