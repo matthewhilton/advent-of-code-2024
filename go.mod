4374b459f9c5f5467afabd95e96f81a0fab0c80c
Matthew Hilton <mj.hilton@outlook.com>
2024-12-08 00:22:37 +1000

    day 2

--- go.mod
+++ go.mod
@@ -3,3 +3,8 @@ module matt/aoc
 go 1.23.3
 
 require golang.org/x/exp v0.0.0-20241108190413-2d47ceb2692f
+
+require (
+	github.com/samber/lo v1.47.0 // indirect
+	golang.org/x/text v0.16.0 // indirect
+)
--- main.go
+++ main.go
@@ -24,8 +24,142 @@ func check(e error) {
 	}
 }
 
+// Stolen from Rust :)
+type Result[T any] struct {
+	value T
+	err   error
+}
+
+func (r Result[T]) Ok() bool {
+	return r.err == nil
+}
+
 func main() {
-	day_1()
+	//day_1()
+	day_2()
+}
+
+func day_2() {
+	data, err := os.ReadFile("./2/input.txt")
+	check(err)
+	lines := strings.Split(string(data), "\n")
+
+	output_channel := make(chan Result[bool], len(lines))
+
+	for _, line := range lines {
+		report_numbers := parse_report(line)
+		is_report_safe_with_combinations(report_numbers, output_channel)
+	}
+
+	num_safe := 0
+	for range len(lines) {
+		result := <-output_channel
+		if result.Ok() && result.value {
+			num_safe += 1
+		}
+	}
+
+	fmt.Println(num_safe)
+}
+
+func parse_report(report string) []int {
+	var parts = strings.Split(strings.TrimSpace(report), " ")
+	var numbers = make([]int, 0)
+
+	for i := range parts {
+		num, err := strconv.Atoi(parts[i])
+
+		// Bad data.
+		if err != nil {
+			continue
+		}
+
+		numbers = append(numbers, num)
+	}
+	return numbers
+}
+
+func generate_report_combinations(report []int) [][]int {
+	// Make combinations of this report, each with 1 item missing.
+	combinations := make([][]int, 0)
+
+	for idx := range report {
+		// Append a combination but removing the given index.
+		combo := slices.Delete(slices.Clone(report), idx, idx+1)
+		combinations = append(combinations, combo)
+	}
+	return combinations
+}
+
+func is_report_safe_with_combinations(report []int, output chan Result[bool]) {
+	combinations := generate_report_combinations(report)
+	results := make([]bool, 0)
+	for _, combination := range combinations {
+		result := is_safe(combination)
+
+		if result.Ok() {
+			results = append(results, result.value)
+		}
+	}
+
+	// Output is true if any were true
+	// i.e. any combination returned true.
+	output <- Result[bool]{
+		err:   nil,
+		value: slices.Contains(results, true),
+	}
+}
+
+func is_safe(report []int) Result[bool] {
+	differences := make([]int, 0)
+
+	for idx := range len(report) - 1 {
+		differences = append(differences, report[idx+1]-report[idx])
+	}
+
+	// Ensure no zeros (i.e. not ascending or descending)
+	if slices.Contains(differences, 0) {
+		return Result[bool]{
+			err:   nil,
+			value: false,
+		}
+	}
+
+	// Ensure all differences in the expected range.
+	for _, difference := range differences {
+		if Abs(difference) < 1 || Abs(difference) > 3 {
+			return Result[bool]{
+				err:   nil,
+				value: false,
+			}
+		}
+	}
+
+	// Ensure ascendeness and descendedness is ok
+	for idx := range len(differences) - 1 {
+		if sign(differences[idx]) != sign(differences[idx+1]) {
+			return Result[bool]{
+				err:   nil,
+				value: false,
+			}
+		}
+	}
+
+	// All checks passed, good!
+	return Result[bool]{
+		err:   nil,
+		value: true,
+	}
+}
+
+func sign(v int) int {
+	if v < 0 {
+		return -1
+	} else if v > 0 {
+		return 1
+	} else {
+		return 0
+	}
 }
 
 /*
